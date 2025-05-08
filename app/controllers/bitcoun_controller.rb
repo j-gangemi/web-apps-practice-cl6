5fe174fd0c92542fb8968fc438e95293065b43d0
Julia Gangemi <julia.gangemi@kellogg.northwestern.edu>
2025-05-08 15:31:46 +0000

    recreated bitcoin app

---
+++ app/controllers/bitcoun_controller.rb
@@ -0,0 +1,2 @@
+class BitcounController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   # get("/things", :controller => "things", :action => "index")
+  get("/bitcoin", :controller => "bitcoin", :action => "index")
 end
